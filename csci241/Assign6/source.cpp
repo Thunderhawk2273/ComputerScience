134acb694964759a2623415c3cadb341633bf573
Will F <40369858+Thunderhawk2273@users.noreply.github.com>
2020-02-25 14:51:56 -0600

    Update source.cpp

--- csci241/Assign6/source.cpp
+++ csci241/Assign6/source.cpp
@@ -1,8 +1,5 @@
 /*********************************************************************
-   PROGRAM:    CSCI 241 Assignment 6
-   PROGRAMMER: your name
-   LOGON ID:   your z-ID
-   DUE DATE:   due date of assignment
+   PROGRAM:    Application 6
 
    FUNCTION:   This program tests the functionality of the Queue
                class.
