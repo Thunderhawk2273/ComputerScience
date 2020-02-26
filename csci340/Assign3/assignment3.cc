2504bb6a292623c7dd95903c9977d8245d010d0d
Will F <40369858+Thunderhawk2273@users.noreply.github.com>
2020-02-26 10:11:50 -0600

    Update assignment3.cc

--- csci340/Assign3/assignment3.cc
+++ csci340/Assign3/assignment3.cc
@@ -1,10 +1,6 @@
 /****************************************************
 PROGRAMMER: WILLIAM FRERIKS
 
-DUE DATE: FRIDAY, SEPTEMBER 18, 2015
-
-PROGRAM: 2
-
 PURPOSE: To find all non-prime integers and destroy them
 
 ****************************************************/
@@ -29,23 +25,6 @@ Purpose: to search and destroy all non-prime numbers in a set
 *****************************************************/
 void sieve( set<int>& s, int n) {
 
-	set<int>::iterator it;
-	set<int>::iterator del_it;
-
-
-	for( int i = 2; i <= n; i++)			//fill up the set with n integers
-		s.insert(i);
-
-
-	for( it = s.begin(); it!=s.end(); ++it ){
-		del_it = it;
-							//Going throught the set and deleting
-		for( del_it++; del_it!=s.end(); ++del_it ){	// integers
-			if(*del_it % *it == 0)
-			s.erase(del_it);
-		}
-	}
-
 }
 
 
@@ -58,25 +37,7 @@ Purpose: to print all of those prime numbers we just found
 *****************************************************/
 void print_primes( const set<int>& s) {
 
-	set<int>::iterator it;
-
-	int count = 0;
-
-	cout << endl;				//formating
-
-	for(it=s.begin(); it!=s.end(); ++it ){
-		cout << setw(ITEM_W) << *it;
-		count++;
-						//using an iterator to go through the set
-		if (count == NO_ITEMS){		// and to print the prime numbers
-			cout << endl;
-			count = 0;
-		}
-	}
-	cout << endl << endl;			//formating
 }
-
-
 
 
 
